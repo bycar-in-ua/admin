d6f837c2ac739b3464f3761b47afd4afa560f896
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-08-20 06:27:26 +0300

    CarEditor Vuex removed

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import { createStore } from "vuex";
 import library from "./modules/library";
 import auth from "./modules/auth";
-import carEditor from "./modules/carEditor";
 
 export default createStore({
-  modules: { library, auth, carEditor },
+  modules: { library, auth },
 });
--- src/store/modules/carEditor/mutationTypes.js
+++
@@ -1,3 +0,0 @@
-export const UPDATE_CAR = "UPDATE_CAR";
-export const UPDATE_CAR_FIELD = "UPDATE_CAR_FIELD";
-export const UPDATE_IS_EDITED = "UPDATE_IS_EDITED";
