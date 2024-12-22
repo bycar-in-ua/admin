280e296f0379c002efed390bb926c1b0ba676228
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2024-12-22 16:04:14 +0200

    Update TinyMCE

--- src/components/VehicleEditor/DescriptionEditor.vue
+++ src/components/VehicleEditor/DescriptionEditor.vue
@@ -1,37 +1,25 @@
-<template>
-  <editor
-    v-model="vehicleStore.car.description"
-    api-key="bkij2k10jssrm5ez30czeck0fluycxbi0p1watets0n39u8n"
-    :init="{
-      height: 400,
-      menubar: false,
-      skin: darkTheme == '1' ? 'oxide-dark' : '',
-      content_css: darkTheme == '1' ? 'dark' : '',
-    }"
-  />
-</template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import { useMemory } from "@/hooks/useMemory";
 import Editor from "@tinymce/tinymce-vue";
 import { useVehicleStore } from "@/stores/vehicleEditor/vehicle.store";
 
-export default defineComponent({
-  name: "DescriptionEditor",
-  components: {
-    Editor,
-  },
-  setup() {
-    const vehicleStore = useVehicleStore();
-    const memory = useMemory();
+const vehicleStore = useVehicleStore();
+const memory = useMemory();
 
-    const darkTheme = memory.get("darkTheme", false);
-
-    return {
-      vehicleStore,
-      darkTheme,
-    };
-  },
-});
+const darkTheme = memory.get("darkTheme", false);
 </script>
+
+<template>
+  <Editor
+    v-model="vehicleStore.car.description"
+    api-key="bkij2k10jssrm5ez30czeck0fluycxbi0p1watets0n39u8n"
+    :init="{
+      height: 400,
+      plugins: 'media',
+      skin: darkTheme == '1' ? 'oxide-dark' : '',
+      content_css: darkTheme == '1' ? 'dark' : '',
+      iframe_template_callback: (data) =>
+        `<div class='responsive-video-iframe-container'><iframe class='responsive-video-iframe' title='${data.title}' width='${data.width}' height='${data.height}' src='${data.source}'></iframe></div>`,
+    }"
+  />
+</template>
