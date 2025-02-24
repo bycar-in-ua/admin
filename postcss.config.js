fd00d081de63e6dce03aa4ab3335ae08b88f4b28
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2025-02-24 07:56:30 +0200

    Packages update

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import tailwindNesting from "@tailwindcss/nesting";
 import tailwindConfig from "./tailwind.config.js";
 
 export default {
-  plugins: [tailwindNesting, tailwind(tailwindConfig), autoprefixer],
+  plugins: [tailwind(tailwindConfig), autoprefixer],
 };
